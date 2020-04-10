bfaec0e54cef4877983c046189edf2d46b0c7b86
Rasan-hue <62095526+Rasan-hue@users.noreply.github.com>
2020-04-10 08:06:24 +0300

    Added homepage Link
    Because convenience

--- homepage.php
+++ homepage.php
@@ -39,6 +39,7 @@
 
   ?>
       <b>Welcome to Bortfoilio Generator <?php echo $_SESSION['username']; echo $_SESSION['userID'] ?>, check some work from our beloved users >w< !</b><br><br>
+      <a href="homepage.php"><button class="w3-button w3-grey w3-margin-right">HomePage</button></a>
       <a href='profile.php?userID=<?php echo $_SESSION['userID'];?>&&username=<?php echo $_SESSION['username'];?>&&professionID=<?php echo $_SESSION['professionID']?>'><button class="w3-button w3-indigo w3-margin-right">Profile</button></a>
       <a href="logout.php?logout"><button class="w3-button w3-red w3-margin-right">Logout</button></a>
       <a href="upload_page.php"><button class="w3-button w3-yellow">Upload a pic!</button></a>
@@ -50,6 +51,8 @@
 
   ?>
       <b>Welcome to Bortfoilio Generator, check some work from our beloved users >w< !</b><br><br>
+      <a href="homepage.php"><button class="w3-button w3-grey w3-margin-right">HomePage</button></a>
+
       <a href="signIn_Page.php"><button class="w3-button w3-green">Login</button></a>  
       <a href="signUp_Page.php"><button class="w3-button w3-blue">Register</button></a>
       <a href="usersPage.php"><button class="w3-button w3-yellow">Browse users</button></a>
--- usersPage.php
+++ usersPage.php
@@ -19,12 +19,37 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
 </nav>
 
 <!-- Top menu -->
-<div class="w3-top">
-  <div class="w3-white w3-xlarge" style="max-width:1200px;margin:auto">
-    <!-- <div class="w3-button w3-padding-16 w3-left" onclick="w3_open()">☰</div> -->
-    <div class="w3-center w3-padding-16">Our usews UwU !</div>
-  </div>
-</div>
+<header class="w3-center w3-margin-bottom">
+  <h1><b>BORTFOILIO GENERATOR</b></h1>
+
+  <?php
+    session_start();
+    if(isset($_SESSION['username'])){
+
+  ?>
+      <b>Usews pwage UwU <?php echo $_SESSION['username']; echo $_SESSION['userID'] ?>, check some work from our beloved users >w< !</b><br><br>
+      <a href="homepage.php"><button class="w3-button w3-grey w3-margin-right">HomePage</button></a>
+      <a href='profile.php?userID=<?php echo $_SESSION['userID'];?>&&username=<?php echo $_SESSION['username'];?>&&professionID=<?php echo $_SESSION['professionID']?>'><button class="w3-button w3-indigo w3-margin-right">Profile</button></a>
+      <a href="logout.php?logout"><button class="w3-button w3-red w3-margin-right">Logout</button></a>
+      <a href="upload_page.php"><button class="w3-button w3-yellow">Upload a pic!</button></a>
+      <a href="usersPage.php"><button class="w3-button w3-yellow">Browse users</button></a>
+
+  <?php
+    }
+    else {
+
+  ?>
+      <b>Usews pwage UwU </b><br><br>
+      <a href="homepage.php"><button class="w3-button w3-grey w3-margin-right">HomePage</button></a>
+      <a href="signIn_Page.php"><button class="w3-button w3-green">Login</button></a>  
+      <a href="signUp_Page.php"><button class="w3-button w3-blue">Register</button></a>
+      <a href="usersPage.php"><button class="w3-button w3-yellow">Browse users</button></a>
+ <?php
+    }
+    ?>
+
+
+</header>
   
 <!-- !PAGE CONTENT! -->
 <div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
